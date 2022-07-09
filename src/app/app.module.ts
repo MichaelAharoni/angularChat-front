88a733fae9edcca402b7a2579d893582d7b5a261
MichaelAharoni <michaelaharoni001@gmail.com>
2022-07-09 21:49:46 +0300

    added http module, fixed some module issues.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { VideoChatComponent } from './video-chat/video-chat.component';
+import { ContactPickerComponent } from './contact-picker/contact-picker.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BarcodeScannerComponent } from './barcode-scanner/barcode-scanner.component';
+import { BarcodeImgComponent } from './barcode-img/barcode-img.component';
 
+const config: SocketIoConfig = {
+	url: environment.socketUrl, // socket server url;
+	options: {
+		transports: ['websocket']
+	}
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoChatComponent,
+    ContactPickerComponent,
+    BarcodeScannerComponent,
+    BarcodeImgComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    SocketIoModule.forRoot(config),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
