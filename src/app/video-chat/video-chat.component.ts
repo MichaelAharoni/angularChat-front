80585b8aeedb0d109fc343cebd85096f6a58d08b
Alex Yakovlev <1yakovlevalex@gmail.com>
2022-07-10 21:21:51 +0300

    F-102: created the option to toggle between front and read camera in video component

--- src/app/video-chat/video-chat.component.ts
+++ src/app/video-chat/video-chat.component.ts
@@ -1,21 +1,21 @@
 import { SocketService } from './../services/socket.service';
-import { Component, ElementRef, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, ChangeDetectorRef, NgZone } from '@angular/core';
 
 @Component({
   selector: 'video-chat',
   templateUrl: './video-chat.component.html',
-  styleUrls: ['./video-chat.component.scss']
+  styleUrls: ['./video-chat.component.scss'],
 })
 
 export class VideoChatComponent implements OnInit {
 
-  constructor(private socketService: SocketService, private cd: ChangeDetectorRef) { }
+  constructor(private socketService: SocketService, private zone: NgZone) { }
 
   sendRoomName(room: HTMLInputElement) {
     this.socketService.emit('join-room', room.value)
     room.value = ''
   }
-
+  cameraMode = 'environment'
   localStream!: MediaStream
   remoteStream!: MediaStream
   peerConn!: RTCPeerConnection
@@ -31,21 +31,25 @@ export class VideoChatComponent implements OnInit {
   // consider adding facingmode
 
   async createPeerConnection() {
+
+    const cameraOpt = (this.cameraMode === 'front') ? 'user' : 'environment'
     if (!this.localStream) {
-      this.localStream = await navigator.mediaDevices.getUserMedia({
-        video: {
-          frameRate: 24,
-          width: {
-            min: 480, ideal: 720, max: 1280
-          },
-          aspectRatio: 1.33333
-        },
-        audio: true
-      })
+      await this.setLocalStream()
+      // this.localStream = await navigator.mediaDevices.getUserMedia({
+      //   video: {
+      //     facingMode:cameraOpt,
+      //     frameRate: 24,
+      //     width: {
+      //       min: 480, ideal: 720, max: 1280
+      //     },
+      //     aspectRatio: 1.33333
+      //   },
+      //   audio: true
+      // })
     }
 
     this.elContainer.nativeElement.style.display = 'inline'
-    this.elLocalVideo.nativeElement.srcObject = this.localStream
+    // this.elLocalVideo.nativeElement.srcObject =   this.localStream
     this.remoteStream = new MediaStream()
     this.elRemoteVideo.nativeElement.srcObject = this.remoteStream
     this.elRemoteVideo.nativeElement.style.display = 'block'
@@ -81,7 +85,7 @@ export class VideoChatComponent implements OnInit {
 
   async createOffer() {
     await this.createPeerConnection()
-    const offer : RTCSessionDescriptionInit = await this.peerConn.createOffer()
+    const offer: RTCSessionDescriptionInit = await this.peerConn.createOffer()
     await this.peerConn.setLocalDescription(offer)
     this.socketService.emit('store-offer', offer)
   }
@@ -120,10 +124,42 @@ export class VideoChatComponent implements OnInit {
     this.elLocalVideo.nativeElement.srcObject = this.elRemoteVideo.nativeElement.srcObject
     this.elRemoteVideo.nativeElement.srcObject = currLocalSrc
   }
+  async toggleCamera() {
+    this.cameraMode = (this.cameraMode === 'user') ? 'environment' : 'user'
+    await this.setLocalStream()
+    this.zone.run(() => {
+      console.log('enabled time travel');
+    });
 
+  }
   async ngOnInit(): Promise<void> {
+    // const cameraOpt  = (this.cameraMode==='front')?  'user' :  'environment' 
+    // this.localStream = await navigator.mediaDevices.getUserMedia({
+    //   video: {
+    //     facingMode:cameraOpt,
+    //     frameRate: 24,
+    //     width: {
+    //       min: 480, ideal: 720, max: 1280
+    //     },
+    //     aspectRatio: 1.33333
+    //   },
+    //   audio: true
+    // })
+    await this.setLocalStream()
+    this.socketService.on('got-answer', async (answer) => {
+      if (!this.peerConn.currentRemoteDescription) await this.peerConn.setRemoteDescription(answer as RTCSessionDescriptionInit)
+    })
+    this.socketService.on('got-offer', async (offer) => {
+      this.newOffer = offer as RTCSessionDescription
+      this.toggleIsGettingACall(true)
+    })
+    this.socketService.on('got-candidate', async (candidate: RTCIceCandidateInit) => { if (this.peerConn && this.peerConn.remoteDescription?.type) await this.peerConn.addIceCandidate(candidate) })
+  }
+  async setLocalStream(): Promise<void> {
+    const cameraOpt = (this.cameraMode === 'user') ? 'user' : 'environment'
     this.localStream = await navigator.mediaDevices.getUserMedia({
       video: {
+        facingMode: cameraOpt,
         frameRate: 24,
         width: {
           min: 480, ideal: 720, max: 1280
@@ -132,13 +168,14 @@ export class VideoChatComponent implements OnInit {
       },
       audio: true
     })
-    this.socketService.on('got-answer', async (answer) => {
-      if (!this.peerConn.currentRemoteDescription) await this.peerConn.setRemoteDescription(answer as RTCSessionDescriptionInit)
-    })
-    this.socketService.on('got-offer', async (offer) => {
-      this.newOffer = offer as RTCSessionDescription
-      this.toggleIsGettingACall(true)
-    })
-    this.socketService.on('got-candidate', async (candidate : RTCIceCandidateInit) => { if (this.peerConn && this.peerConn.remoteDescription?.type) await this.peerConn.addIceCandidate(candidate) })
+    // this.elContainer.nativeElement.style.display = 'inline'
+    this.elLocalVideo.nativeElement.srcObject =   this.localStream
+    // this.remoteStream = new MediaStream()
+    // this.elRemoteVideo.nativeElement.srcObject = this.remoteStream
+    // this.elRemoteVideo.nativeElement.style.display = 'block'
+
+
   }
+  
+
 }
